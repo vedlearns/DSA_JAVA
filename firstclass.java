182c3ed12f58d285a662e9cd5474fb95e6e55c9a
vedlearns <Vedankxb@gmail.com>
2024-09-04 22:29:47 +0530

    updates

--- firstclass.java
+++ firstclass.java
@@ -1,12 +1,7 @@
+
 public class firstclass {
+
     public static void main(String[] args) {
-        for(int i=0;i<10;i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                System.out.print("* ");
-            }
-            System.out.println(" ");
-        }
+        System.out.println("hello");
     }
 }
--- patterns.java
+++ patterns.java
@@ -1,7 +1,12 @@
-
 public class patterns {
-
     public static void main(String[] args) {
-        System.out.println("hello");
+        for(int i=0;i<10;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
     }
 }
