2d96f90276cca202352d4fb83e69828a7118d15b
vedlearns <Vedankxb@gmail.com>
2024-09-06 02:49:24 +0530

    changes added

--- patterns.java
+++ patterns.java
@@ -2,7 +2,7 @@ public class patterns {
     public static void main(String[] args) {
         for(int i=0;i<10;i++)
         {
-            for(int j=0;j<i;j++)
+            for(int j=0;j<10;j++)
             {
                 System.out.print("* ");
             }
