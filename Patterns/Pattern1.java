a0ffef867575e9099c10b323acd2acb4d3dbc9fe
vedlearns <Vedankxb@gmail.com>
2024-09-21 04:17:37 +0530

    patterns started

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,21 @@
+package Patterns;
+public class Pattern1 {
+    public static void main(String[] args) 
+    {
+        // void p1(int n)
+        // {
+            for(int i=0;i<5;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        // }
+        
+
+        
+    }
+   
+}
--- firstclass.java
+++ firstclass.java
@@ -3,5 +3,28 @@ public class firstclass {
 
     public static void main(String[] args) {
         System.out.println("hello ");
+//         int i =1;
+// // if(i==0)
+// {
+//     System.out.println("yes");
+// }
+
+// 
+//<--checking switch case-->
+//switch (8)// variable ke sath actual value bhi daal skte ho
+// {
+//     // agr Case nhi rhega to kuch run nhi hoga , par agr ek bhi case match hogya uske neeche ke sb run honge 
+//     case 0:
+//         System.out.println(0);
+//         break;
+//     case 1:
+//         System.out.println("2");
+//         break;
+    
+//     default:
+//     System.out.println("k");
+    
+// }
+
     }
 }
