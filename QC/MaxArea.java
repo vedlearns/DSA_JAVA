b8770dd1a0203bf623d423bf4a13d776dd8b76ba
vedlearns <Vedankxb@gmail.com>
2024-10-12 17:35:52 +0530

    Patterns completed

--- QC/MaxArea.java
+++ QC/MaxArea.java
@@ -37,3 +37,23 @@ public class MaxArea{
         System.out.println("Max Area of the Container can be "+ max);
     }
 }
+
+//OPTIMUM SOLN
+//public int maxArea(int[] height) {
+//    int left = 0;
+//    int right = height.length - 1;
+//    int maxArea = 0;
+//
+//    while (left < right) {
+//        int currentArea = Math.min(height[left], height[right]) * (right - left);
+//        maxArea = Math.max(maxArea, currentArea);
+//
+//        if (height[left] < height[right]) {
+//            left++;
+//        } else {
+//            right--;
+//        }
+//    }
+//
+//    return maxArea;
+//}
